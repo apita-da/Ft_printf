d9d5c0afed455434e84fb4f5fbefae9711afdcfc
Amalia <amaliapdvh@gmail.com>
2020-09-29 00:59:37 +0200

    Feat: prec, width broken

--- ft_put_str.c
+++ ft_put_str.c
@@ -4,37 +4,56 @@ void	ft_put_str(t_struct *s)
 {
 	int				len;
 	const char		*str;
+	int				prec;
 
 	str = va_arg(s->argv, const char *);
-	len = ft_strlenprint(str);
-	
-	
-	if(len >= s->flag.width && len >= s->flag.prec)
-		ft_putstr(str, s);
-	else
+	prec = s->flag.prec;
+	len = ft_strlenprint(str, prec);
+	if (s->flag.prec)
 	{
-		s->flag.width = 1 + s->flag.width - len;
+		
+		s->flag.width = 1 + s->flag.width - s->flag.prec;
 		if (s->flag.minus == 1)
 		{
 				ft_putstr(str, s);
 				while(--s->flag.width)
 				{
 					write(1, " ", 1);
-				  	s->ret++;
+					s->ret++;
 				}
 		}
 		else
 		{
-			if (s->flag.prec)
+			while(--s->flag.width)
 			{
-				while(--s->flag.width)
-				{
-					write(1, " ", 1);
-					s->ret++;
-				}
-				ft_putstr(str, s);
-				s->ret--;
+				write(1, " ", 1);
+				s->ret++;
 			}
+			ft_putstr(str, s);						
+		}
+	}
+	
+	else
+	{
+		s->flag.width = 1 + s->flag.width - len;
+		if (s->flag.minus == 1)
+		{
+			ft_putstr(str, s);
+			while(--s->flag.width)
+			{
+				write(1, "a", 1);
+				s->ret++;
+			}
+		}
+		else
+		{
+			while(--s->flag.width)
+			{
+				write(1, " ", 1);
+				s->ret++;
+			}
+			ft_putstr(str, s);						
 		}
 	}
 }
+	
--- ft_putstr.c
+++ ft_putstr.c
@@ -8,16 +8,21 @@ void	ft_putstr(const char *str, t_struct *s)
 {
 	int	i;
 
-	i = 0;
-
+	i = -1;
+	if(!s->flag.prec)
 	{
-		while (str[i] != '\0' && s->flag.prec)
+		while (str[++i] != '\0')
 		{
 			ft_putchar(str[i]);
-			i++;
+		}
+	}
+	else
+	{
+		while (str[++i] != '\0' && s->flag.prec)
+		{
+			ft_putchar(str[i]);
 			s->flag.prec--;
 			s->ret++;
 		}
-		s->ret++;
 	}
 }
