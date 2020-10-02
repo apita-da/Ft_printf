ea23fe2be36c643b7235eaf2bb97dcfc95f3bd0c
Amalia <amaliapdvh@gmail.com>
2020-10-03 00:31:13 +0200

    try pointer

--- ft_put_p.c
+++ ft_put_p.c
@@ -14,60 +14,87 @@
 
 void	ft_put_minus_p(t_struct *s)
 {
-	unsigned long int	c;
-	int					width;
-	int					len;
+// 	unsigned long int	c;
+// 	int					width;
+// 	int					len;
 
-	c = (long)va_arg(s->argv, void *);
-	width = s->flag.width;
-	len = ft_count_hex(c);
-	width = width - (len + 2);
-	if (len > width)
-	{
-		ft_putstr("0x", s);
-		ft_putnbrbase(c, "0123456789abcdef", s);
-	}
-	else
-	{
-		ft_putstr("0x", s);
-		ft_putnbrbase(c, "0123456789abcdef", s);
-		while (width--)
-		{
-			ft_putchar(' ', s);
-		}
-	}
-}
+// 	c = (long)va_arg(s->argv, void *);
+// 	width = s->flag.width;
+// 	len = ft_count_hex(c);
+// 	width = width - (len + 2);
+// 	if (len > width)
+// 	{
+// 		ft_putstr("0x", s);
+// 		ft_putnbrbase(c, "0123456789abcdef", s);
+// 	}
+// 	else
+// 	{
+// 		ft_putstr("0x", s);
+// 		ft_putnbrbase(c, "0123456789abcdef", s);
+// 		while (width--)
+// 		{
+// 			ft_putchar(' ', s);
+// 		}
+// 	}
+// }
 
-void	ft_put_width_p(t_struct *s)
-{
-	unsigned long int	c;
-	int					width;
-	int					len;
+// void	ft_put_width_p(t_struct *s)
+// {
+// 	unsigned long int	c;
+// 	int					width;
+// 	int					len;
 
-	c = (long)va_arg(s->argv, void *);
-	width = s->flag.width;
-	len = ft_count_hex(c);
-	width = width - (len + 2);
-	if (len > width)
-	{
-		ft_putstr("0x", s);
-		ft_putnbrbase(c, "0123456789abcdef", s);
-	}
-	else
-	{
-		while (width--)
-		{
-			ft_putchar(' ', s);
-		}
-		ft_putstr("0x", s);
-		ft_putnbrbase(c, "0123456789abcdef", s);
-	}
-}
+// 	c = (long)va_arg(s->argv, void *);
+// 	width = s->flag.width;
+// 	len = ft_count_hex(c);
+// 	width = width - (len + 2);
+// 	if (len > width)
+// 	{
+// 		ft_putstr("0x", s);
+// 		ft_putnbrbase(c, "0123456789abcdef", s);
+// 	}
+// 	else
+// 	{
+// 		while (width--)
+// 		{
+// 			ft_putchar(' ', s);
+// 		}
+// 		ft_putstr("0x", s);
+// 		ft_putnbrbase(c, "0123456789abcdef", s);
+// 	}
+// }
 
+// void	ft_put_pointer(t_struct *s)
+// {
+// 	if (s->flag.minus == 1)
+// 		ft_put_minus_p(s);
+// 	else
+// 		ft_put_width_p(s);
+// }
 void	ft_put_pointer(t_struct *s)
 {
-	if (s->flag.minus == 1)
-		ft_put_minus_p(s);
-	else
-		ft_put_width_p(s);
+	unsigned long int	c;
+	int					len;
+	char				space;
+
+	c = (long)va_arg(s->argv, void *);
+	len = ft_count_hex(c);
+	if (c == 0 && s->flag.prec_zero == 1)
+		len = 0;
+	s->flag.width = s->flag.width - (len > s->flag.prec ? len : s->flag.prec);
+	s->flag.prec = s->flag.prec - len;
+	space = ' ';
+	if (s->flag.zero == 1)
+		space = '0';
+	while (s->flag.minus == -1 && s->flag.width-- > 0)
+		ft_putchar(space, s);
+	while (s->flag.prec-- > 0)
+		ft_putchar('0', s);
+	if (!(s->flag.prec_zero == 1 && c == 0))
+	{
+		ft_putstr("0x", s);
+		ft_putnbrbase(c, "0123456789abcdef", s);
+	}
+	while (s->flag.minus == 1 && s->flag.width-- > 0)
+		ft_putchar(space, s);
 }
