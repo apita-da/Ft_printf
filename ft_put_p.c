1251ec6125b4907afb55d3ee1b494efd66088af0
Amalia <amaliapdvh@gmail.com>
2020-10-03 13:58:15 +0200

    30 try

--- ft_put_p.c
+++ ft_put_p.c
@@ -64,29 +64,40 @@ void	ft_put_width_p(t_struct *s)
 	}
 }
 
+// void	ft_put_pointer(t_struct *s)
+// {
+// 	if (s->flag.prec != 0)
+// 		s->flag.zero = 1;
+// 	if (s->flag.minus == 1)
+// 		ft_put_minus_p(s);
+// 	else
+// 		ft_put_width_p(s);
+// }
+
 void	ft_put_pointer(t_struct *s)
 {
 	unsigned long int	c;
+	int					len;
+	char				space;
 
-	c = (long)va_arg(s->argv, void *);
-	if (!c && s->flag.width < 2)
-	{
-		if (s->flag.prec == 0 && s->flag.width < 2)
-			s->flag.width = 2;
-		ft_putstr("0x0", s);
-		return ;
-	}
-	else
-	{
-		ft_putstr("0x", s);
-		ft_putnbrbase(c, "0123456789abcdef", s);
-	}
-	if (s->flag.prec == 0 && s->flag.width < ft_count_hex(c))
-		s->flag.width = ft_count_hex(c);
-	if (s->flag.prec != 0)
-		s->flag.zero = 1;
-	if (s->flag.minus == 1)
-		ft_put_minus_p(s);
-	else
-		ft_put_width_p(s);
+	c = (unsigned int)va_arg(s->argv, unsigned int);
+	len = ft_count_hex(c);
+	if (c == 0 && s->flag.prec_zero == 1)
+		len = 0;
+	s->flag.width = s->flag.width - (len > s->flag.prec ? len : s->flag.prec);
+	s->flag.prec = s->flag.prec - len;
+	space = ' ';
+	if (s->flag.zero == 1)
+		space = '0';
+	while (s->flag.minus == -1 && s->flag.width-- > 0)
+		ft_putchar(space, s);
+	while (s->flag.prec-- > 0)
+		ft_putchar('0', s);
+	if (!(s->flag.prec_zero == 1 && c == 0))
+		{
+			ft_putstr("0x", s);
+			ft_putnbrbase(c, "0123456789abcdef", s);
+		}
+	while (s->flag.minus == 1 && s->flag.width-- > 0)
+		ft_putchar(space, s);
 }
