c549e1b563334fb427b568c68cf8f820cf6be7b2
Amalia <amaliapdvh@gmail.com>
2020-10-03 12:49:25 +0200

    fix p prec?

--- ft_put_p.c
+++ ft_put_p.c
@@ -71,11 +71,13 @@ void	ft_put_pointer(t_struct *s)
 	c = (long)va_arg(s->argv, void *);
 	if (!c && s->flag.width < 2)
 	{
-		if (s->flag.prec_zero == 1 && s->flag.width < 2)
+		if (s->flag.prec = 0 && s->flag.width < 2)
 			s->flag.width = 2;
 		ft_putstr("0x0", s);
 		return ;
 	}
+	if (s->flag.prec != 0)
+		s->flag.zero = 1;
 	if (s->flag.minus == 1)
 		ft_put_minus_p(s);
 	else
