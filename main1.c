66705d59049e357789e8dacde10dd8df7d43be77
Amalia <amaliapdvh@gmail.com>
2020-10-04 13:46:47 +0200

    try 2 prec pinteer

--- main1.c
+++ main1.c
@@ -11,9 +11,9 @@ int		main(void)
     // printf("\tret mio = %i\n", ret);
     // ret2 = printf("%16p%16p%16p%16p",&ret,&ret2,&ret,&ret2);
     // printf("\tret bueno = %i\n", ret2);
-    ret = ft_printf("%.*s", -3, "yolo");
+    ret = ft_printf("%i", 0);
     printf("\tret mio = %i\n", ret);
-    ret2 = printf("%.*s",-3, "yolo");
+    ret2 = printf("%i", 0);
     printf("\tret bueno = %i\n", ret2);
 	//ret = ft_printf("%-15.10s", "123456");
     // printf("\t ret mio = %i\n", ret);
