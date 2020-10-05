7e2c720a223e2f1a22718e5217d79f9310759baa
Amalia <amaliapdvh@gmail.com>
2020-10-05 12:03:19 +0200

    Try p

--- ft_put_p.c
+++ ft_put_p.c
@@ -12,66 +12,6 @@
 
 #include "libftprintf.h"
 
-void	ft_put_minus_p(t_struct *s)
-{
-	unsigned long int	c;
-	int					width;
-	int					len;
-
-	c = (long)va_arg(s->argv, void *);
-	width = s->flag.width;
-	len = ft_count_hex(c);
-	width = width - (len + 2);
-	if (len > width)
-	{
-		ft_putstr("0x", s);
-		ft_putnbrbase(c, "0123456789abcdef", s);
-	}
-	else
-	{
-		ft_putstr("0x", s);
-		ft_putnbrbase(c, "0123456789abcdef", s);
-		while (width-- > 0)
-		{
-			ft_putchar(' ', s);
-		}
-	}
-}
-
-void	ft_put_width_p(t_struct *s)
-{
-	unsigned long int	c;
-	int					width;
-	int					len;
-
-	c = (long)va_arg(s->argv, void *);
-	width = s->flag.width;
-	len = ft_count_hex(c);
-	width = width - (len + 2);
-	if (len > width)
-	{
-		ft_putstr("0x", s);
-		ft_putnbrbase(c, "0123456789abcdef", s);
-	}
-	else
-	{
-		while (width-- > 0)
-		{
-			ft_putchar(' ', s);
-		}
-		ft_putstr("0x", s);
-		ft_putnbrbase(c, "0123456789abcdef", s);
-	}
-}
-
- /*void	ft_put_pointer(t_struct *s)
-{
- 	if (s->flag.minus == 1)
- 		ft_put_minus_p(s);
- 	else
- 		ft_put_width_p(s);
- }
-*/
 void	ft_put_pointer(t_struct *s)
 {
 	unsigned long int	c;
@@ -79,6 +19,8 @@ void	ft_put_pointer(t_struct *s)
 	char				space;
 
 	c = (unsigned long)va_arg(s->argv, unsigned long);
+	if (!c || c == '0')
+		ft_putstr("0x", s);
 	len = ft_count_hex(c) + 2;
 	s->flag.width = s->flag.width - (len > s->flag.prec ? len : s->flag.prec);
 	s->flag.prec = s->flag.prec - len;
