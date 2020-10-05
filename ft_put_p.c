054b63dd9f913283d6b3bc66880e431e0727ddef
Amalia <amaliapdvh@gmail.com>
2020-10-05 11:57:51 +0200

    Try p

--- ft_put_p.c
+++ ft_put_p.c
@@ -25,11 +25,15 @@ void	ft_put_minus_p(t_struct *s)
 	if (len > width)
 	{
 		ft_putstr("0x", s);
+		if (--s->flag.prec > 0)
+			ft_putchar('0', s);
 		ft_putnbrbase(c, "0123456789abcdef", s);
 	}
 	else
 	{
 		ft_putstr("0x", s);
+		if (--s->flag.prec > 0)
+			ft_putchar('0', s);
 		ft_putnbrbase(c, "0123456789abcdef", s);
 		while (width-- > 0)
 		{
@@ -51,6 +55,8 @@ void	ft_put_width_p(t_struct *s)
 	if (len > width)
 	{
 		ft_putstr("0x", s);
+		if (--s->flag.prec > 0)
+			ft_putchar('0', s);
 		ft_putnbrbase(c, "0123456789abcdef", s);
 	}
 	else
@@ -60,37 +66,40 @@ void	ft_put_width_p(t_struct *s)
 			ft_putchar(' ', s);
 		}
 		ft_putstr("0x", s);
+		if (--s->flag.prec > 0)
+			ft_putchar('0', s);
 		ft_putnbrbase(c, "0123456789abcdef", s);
 	}
 }
 
- /*void	ft_put_pointer(t_struct *s)
+void	ft_put_pointer(t_struct *s)
 {
  	if (s->flag.minus == 1)
  		ft_put_minus_p(s);
  	else
  		ft_put_width_p(s);
- }
-*/
-void	ft_put_pointer(t_struct *s)
-{
-	unsigned long int	c;
-	int					len;
-	char				space;
-
-	c = (unsigned long)va_arg(s->argv, unsigned long);
-	len = ft_count_hex(c) + 2;
-	s->flag.width = s->flag.width - (len > s->flag.prec ? len : s->flag.prec);
-	s->flag.prec = s->flag.prec - len;
-	space = ' ';
-	if (s->flag.zero == 1)
-		space = '0';
-	while (s->flag.minus == -1 && s->flag.width-- > 0)
-		ft_putchar(space, s);
-	ft_putstr("0x", s);
-	while ((s->flag.prec-- > 0) || (s->flag.prec > s->flag.width))
-		ft_putchar('0', s);
-	ft_putnbrbase(c, "0123456789abcdef", s);
-	while (s->flag.minus == 1 && s->flag.width-- > 0)
-		ft_putchar(space, s);
 }
+// void	ft_put_pointer(t_struct *s)
+// {
+// 	unsigned long int	c;
+// 	int					len;
+// 	char				space;
+
+// 	c = (unsigned long)va_arg(s->argv, unsigned long);
+// 	if (!c || c == '0')
+// 		ft_putstr("0x", s);
+// 	len = ft_count_hex(c) + 2;
+// 	s->flag.width = s->flag.width - (len > s->flag.prec ? len : s->flag.prec);
+// 	s->flag.prec = s->flag.prec - len;
+// 	space = ' ';
+// 	if (s->flag.zero == 1)
+// 		space = '0';
+// 	while (s->flag.minus == -1 && s->flag.width-- > 0)
+// 		ft_putchar(space, s);
+// 	ft_putstr("0x", s);
+// 	while ((s->flag.prec-- > 0) || (s->flag.prec > s->flag.width))
+// 		ft_putchar('0', s);
+// 	ft_putnbrbase(c, "0123456789abcdef", s);
+// 	while (s->flag.minus == 1 && s->flag.width-- > 0)
+// 		ft_putchar(space, s);
+// }
